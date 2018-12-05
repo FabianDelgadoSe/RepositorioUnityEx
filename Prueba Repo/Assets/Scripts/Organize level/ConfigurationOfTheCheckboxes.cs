527b4aa9432313dcb03af154bb6741408cc3097d
Kevin Castellanos <kcastellanos278@unab.edu.co>
2018-12-04 23:27:39 -0500

    la escena tiene una cuadricula que al dar play cambia de color al igual que un boton con el cual puede cambiar de colo

---
+++ Prueba Repo/Assets/Scripts/Organize level/ConfigurationOfTheCheckboxes.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Esta clase tiene el proposito de contener todas las funciones utilizadas para 
+/// la configuracion del tablero.
+/// </summary>
+public class ConfigurationOfTheCheckboxes : MonoBehaviour {
+    
+    private GameObject[] _boardSquares;
+
+    [SerializeField] private Sprite _boardSquareBlue;
+    [SerializeField] private Sprite _boardSquareRed;
+    [SerializeField] private Sprite _boardSquareYellow;
+    [SerializeField] private Sprite _boardSquareGreen;
+
+    /// <summary>
+    /// Este metodo start es usado para llamar diferentes metodos para realizar la configuracion inicial
+    /// del tablero.
+    /// </summary>
+    void Start () {
+        findBoardSquares();
+        changeColorBoardSquares();
+	}// cierre de la funcion Start 
+	
+	
+
+    /// <summary>
+    /// Metodo encargado de buscar todas las casillas del tablero.
+    /// </summary>
+    private void findBoardSquares()
+    {
+        _boardSquares = GameObject.FindGameObjectsWithTag("CheckBoxes");
+
+    }//cierre de la funcion findBoardSquares
+
+    /// <summary>
+    /// Metodo encargado de cambiar el color a cada una de las diferentes casillas 
+    /// </summary>
+    public void changeColorBoardSquares()
+    {
+        int _codeColor;
+
+        for (int i = 0; i<_boardSquares.Length; i++)
+        {
+            _codeColor = Random.RandomRange(1, 5);
+
+            switch (_codeColor)
+            {
+                case 1:
+                    _boardSquares[i].GetComponent<SpriteRenderer>().sprite = _boardSquareBlue;
+                    break;
+
+                case 2:
+                    _boardSquares[i].GetComponent<SpriteRenderer>().sprite = _boardSquareGreen;
+                    break;
+
+                case 3:
+                    _boardSquares[i].GetComponent<SpriteRenderer>().sprite = _boardSquareRed;
+                    break;
+
+                case 4:
+                    _boardSquares[i].GetComponent<SpriteRenderer>().sprite = _boardSquareYellow;
+                    break;
+
+                default:
+                    Debug.Log("salio del rango " + _codeColor);
+                    break;
+
+            }//cierre switch que escoge colores
+
+        }//cierre for que recorre el arreglo
+
+    } //cierre de la funcion changeColorBoardSquares
+
+}// cierre de la clase 
