71cbc3f6d14f1428df0929b4c00841a0db6f8615
FabianDelgadoSe <45413583+FabianDelgadoSe@users.noreply.github.com>
2018-12-17 07:16:43 -0500

    Texto de contador de players en el lobby
    Se añadio el plugin de TextMeshPRo.
    Se creo la clase LobbyManager

---
+++ Prueba Repo/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LobbyManager : Photon.PunBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI playersCountText;
+    Room _currentRoom;
+
+    public TextMeshProUGUI PlayersCountText
+    {
+        get
+        {
+            return playersCountText;
+        }
+
+        set
+        {
+            playersCountText = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Obtiene la variable del NetworkManager para extraer el playercount y mandarlo al txt
+    [PunRPC]
+    public void showPlayersConnected()
+    {
+        Debug.Log("Tomar room del networkmanager");
+        _currentRoom = this.GetComponent<NetworkManager>().currentRoom;
+        Debug.Log("Mostrar player conectados");
+        playersCountText.text = "PlayerCount\n " + _currentRoom.PlayerCount.ToString();
+
+
+
+    }
+}
--- Prueba Repo/Assets/Scripts/NetworkManager.cs
+++ Prueba Repo/Assets/Scripts/NetworkManager.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 /// <summary>
 /// Clase nesesaria para toda la configuracion cuando se conecta con el servidor
 /// </summary>
-public class NetworkManager : Photon.MonoBehaviour {
+public class NetworkManager : Photon.MonoBehaviour
+{
 
     public string _version;
+    public Room currentRoom;
+
+
     /// <summary>
     /// se conecta con el servidor con la version que se le pase
     /// </summary>
-	void Start () {
+	void Start()
+    {
         PhotonNetwork.ConnectUsingSettings(_version);
     }
 
@@ -21,8 +27,8 @@ public class NetworkManager : Photon.MonoBehaviour {
     void OnConnectedToMaster()
     {
         // nombre de la sala, opciones de la sala y algo que no se que es
-        PhotonNetwork.JoinOrCreateRoom("unica",new RoomOptions() { maxPlayers = 6}, null);
-        
+        PhotonNetwork.JoinOrCreateRoom("unica", new RoomOptions() { maxPlayers = 6 }, null);
+
     }
 
     /// <summary>
@@ -37,11 +43,29 @@ public class NetworkManager : Photon.MonoBehaviour {
     /// Se llama cuando se entra a una sala,
     /// contiene una funcion para que se sincroniza todas las scenas de la room
     /// </summary>
-    void OnJoinedRoom() 
+    void OnJoinedRoom()
     {
+        if (PhotonNetwork.room != null)
+        {
+            Debug.Log("GuardandoRoom");
+            currentRoom = PhotonNetwork.room;
+        }
+
         Debug.Log("entre a la sala");
+
+        //Evalua que haya un campo de texto que llenar
+        if (this.GetComponent<LobbyManager>().PlayersCountText != null) {
+
+            //llama la funcion de la clase LobbyManager
+            this.GetComponent<PhotonView>().RPC("showPlayersConnected", PhotonTargets.All);
+
+        }
+
         PhotonNetwork.automaticallySyncScene = true;
+
+
     }
 
- 
+
+
 }
