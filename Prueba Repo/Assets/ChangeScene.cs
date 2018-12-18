b086a6543267a88e99f3093811ab200a131b1638
FabianDelgadoSe <45413583+FabianDelgadoSe@users.noreply.github.com>
2018-12-17 21:45:57 -0500

    Revert "ya el tablero genera muros y al ser un cuarto sincronico un cambio de escena normal basta"
    This reverts commit 1eb97d27594d52e7cf743d220fddc50b08ad1656.

--- Prueba Repo/Assets/ChangeScene.cs
+++ Prueba Repo/Assets/ChangeScene.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Scrip provicional hasta que se cree correctamente el paso de escenas
 /// </summary>
-public class ChangeScene : MonoBehaviour {
+public class ChangeScene : Photon.PunBehaviour {
 
 
     public void chansy()
     {
-        SceneManager.LoadScene("Main");
+        photonView.RPC("change", PhotonTargets.All);
     }
 
+
+    [PunRPC]
+    public void change()
+    {
+        SceneManager.LoadScene("Main");
+    }
        
 }
