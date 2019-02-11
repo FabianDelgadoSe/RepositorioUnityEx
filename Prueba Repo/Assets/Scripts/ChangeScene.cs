ed9db890cb54b8aee8717822ff6aef86420adf61
FabianDelgadoSe <fabian.delgadose@hotmail.es>
2019-02-11 00:14:27 -0500

    Mejora sistame al cambio de escenas
    Se añadieron constante y enums al script de cambio de escena

--- Prueba Repo/Assets/Scripts/ChangeScene.cs
+++ Prueba Repo/Assets/Scripts/ChangeScene.cs
@@ -5,17 +5,50 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Scrip provicional hasta que se cree correctamente el paso de escenas
 /// </summary>
-public class ChangeScene : Photon.PunBehaviour {
+public class ChangeScene : Photon.PunBehaviour
+{
 
+    private const string LOBBY = "Lobby";
+    private const string MAIN = "Main";
+    private const string PRELOBBY = "Pre-Lobby";
+    private const string SELECTROOM = "SelectRoom";
+
+    public enum Scenes
+    {
+        none, Lobby, Main, PreLobby, SelectRoom
+    }
+
+    public Scenes _sceneToChange = Scenes.none;
 
     public void chansy()
     {
-        photonView.RPC("changeScene", PhotonTargets.All);
+    
+        switch (_sceneToChange)
+        {
+            case Scenes.Lobby:
+                photonView.RPC("changeScene", PhotonTargets.All, LOBBY);
+                break;
+            case Scenes.Main:
+                photonView.RPC("changeScene", PhotonTargets.All, MAIN);
+                break;
+            case Scenes.PreLobby:
+                photonView.RPC("changeScene", PhotonTargets.All, PRELOBBY);
+                break;
+            case Scenes.SelectRoom:
+                photonView.RPC("changeScene", PhotonTargets.All, SELECTROOM);
+                break;
+            case Scenes.none:
+                //AÑADIR FEEDBACK DE NINGUNA SELECCIÓN
+                break;
+
+        }
+
+        //photonView.RPC("changeScene", PhotonTargets.All);
     }
 
     [PunRPC]
-    void changeScene()
+    void changeScene(string sceneName)
     {
         SceneManager.LoadScene("Main");
-    }   
+    }
 }
